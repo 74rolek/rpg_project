287cb812e4363327d7a3e279af79e7bbb24c4fc3
Karol <karolbulicz4@gmail.com>
2026-07-23 21:17:18 +0200

    Zrobiłem statystyki. Jutro zrobię ogniska i widget rozdawania statystyk.

--- Source/EldenRings/GameInstance_MOJ.cpp
+++ Source/EldenRings/GameInstance_MOJ.cpp
@@ -1,8 +1,117 @@
-#include "MojGameInstance.h"
+#include "GameInstance_MOJ.h"
 
-void UMojGameInstance::DodajXP(int32 Ilosc)
+void UGameInstance_MOJ::DodajXP(int Ilosc)
 {
 	AktualnyXP += Ilosc;
+}
 
-	UE_LOG(LogTemp, Warning, TEXT("Dodano %d XP! Aktualny stan: %d"), Ilosc, AktualnyXP);
+void UGameInstance_MOJ::Wyzeruj_XP()
+{
+	AktualnyXP = 0;
+}
+
+int UGameInstance_MOJ::Daj_mi_wymagane_XP_do_ulepszenia_postaci()
+{
+	float BazowyKoszt = 100.0f;
+	float Koszt = BazowyKoszt + FMath::Pow(static_cast<float>(Level), 1.7f) * 35.0f;
+
+	return FMath::RoundToInt(Koszt);
+}
+
+void UGameInstance_MOJ::PodniesLevel(int Ilosc)
+{
+	Level += Ilosc;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykeSila()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Sila++;
+		Obrazenia_Fizyczne += 2;
+		return true;
+	}
+	return false;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykeWitalnosc()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Witalnosc++;
+		Maksymalne_HP += 25;
+		HP = Maksymalne_HP;
+		return true;
+	}
+	return false;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykeZrecznosc()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Zrecznosc++;
+		Obrazenia_Fizyczne += 1;
+		return true;
+	}
+	return false;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykeMana()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Mana++;
+		Maksymalna_Mana += 15;
+		Statystyka_Mana = Maksymalna_Mana;
+		return true;
+	}
+	return false;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykeInteligencja()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Inteligencja++;
+		Obrazenia_Magiczne += 3;
+		return true;
+	}
+	return false;
+}
+
+bool UGameInstance_MOJ::UlepszStatystykePoise()
+{
+	int WymaganyXP = Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	if (AktualnyXP >= WymaganyXP)
+	{
+		AktualnyXP -= WymaganyXP;
+		PodniesLevel(1);
+		Poise_Build++;
+		Maksymalne_Poise += 5;
+		Poise = Maksymalne_Poise;
+		return true;
+	}
+	return false;
 }
--- Source/EldenRings/GameInstance_MOJ.h
+++ Source/EldenRings/GameInstance_MOJ.h
@@ -2,47 +2,153 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-#include "MojGameInstance.generated.h"
+#include "GameInstance_MOJ.generated.h"
 
 UCLASS()
-class TWOJANAZWAPROJEKTU_API UMojGameInstance : public UGameInstance
+class ELDENRINGS_API UGameInstance_MOJ : public UGameInstance
 {
 	GENERATED_BODY()
 
 public:
-	// Build_postaci
+
+	// --- BUILD POSTACI ---
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
-	int Sila;
+	int Sila = 10;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
-	int Witalnosc;
+	int Witalnosc = 10;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
-	int Zrecznosc;
+	int Zrecznosc = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
+	int Mana = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
+	int Poise_Build = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build_postaci")
+	int Inteligencja = 10;
 
 
-	//Statystyki
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int32 AktualnyXP = 0;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int Level;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int Mana;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int Maksymalna_Mana;
+	// --- STATYSTYKI ---
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int HP;
+	int Level = 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
-	int Maksymalne_HP;
+	int AktualnyXP = 0;
 
-	// Funkcja do dodawania XP dostępna z poziomu Blueprintów
+	// Zdrowie
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int HP = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Maksymalne_HP = 100;
+
+	// Mana
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Statystyka_Mana = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Maksymalna_Mana = 50;
+
+	// Wytrzymałość
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Wytzymalosc = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Maksymalna_Wytzymalosc = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Regeneracja_Wytrzymalosci = 30;
+
+	// Poise (Równowaga)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Poise = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Statystyki")
+	int Maksymalne_Poise = 30;
+
+
+	// --- OBRAŻENIA ---
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Fizyczne = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Magiczne = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Obrazajace = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Ogniste = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Zmarzniecie = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Krwawienie = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obrażenia")
+	int Obrazenia_Zatrucie = 0;
+
+
+	// --- ODPORNOŚCI ---
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Fizyczna = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Magiczna = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Obrazajaca = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Ognista = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Zmarzniecie = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Krwawienie = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Odporności")
+	int Odpornosc_Zatrucie = 0;
+
+
+	// --- FUNKCJE ---
+
 	UFUNCTION(BlueprintCallable, Category = "Statystyki")
-	void DodajXP(int32 Ilosc);
+	void DodajXP(int Ilosc);
+
+	UFUNCTION(BlueprintCallable, Category = "Statystyki")
+	void Wyzeruj_XP();
+
+	UFUNCTION(BlueprintCallable, Category = "Statystyki")
+	int Daj_mi_wymagane_XP_do_ulepszenia_postaci();
+
+	UFUNCTION(BlueprintCallable, Category = "Statystyki")
+	void PodniesLevel(int Ilosc);
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykeSila();
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykeWitalnosc();
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykeZrecznosc();
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykeMana();
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykeInteligencja();
+
+	UFUNCTION(BlueprintCallable, Category = "Build_postaci")
+	bool UlepszStatystykePoise();
 };
